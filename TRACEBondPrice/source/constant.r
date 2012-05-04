cc5ff1f7a92e4dafa9a04f7e2d20ea66224bc5da
gentinex <gentinex@yahoo.com>
2012-05-04 09:04:21 -0500

    added constant routine

---
+++ TRACEBondPrice/source/constant.r
@@ -0,0 +1,8 @@
+##### Constant
+# just set the price equal to the last traded price
+
+Constant <- function(){
+  test <-read.csv( "../data/test.csv",  header = TRUE, na.strings = "NA")
+  predictions_df <- data.frame(id=test$id, trace_price=test$trade_price_last1)
+  return(predictions_df)
+}
