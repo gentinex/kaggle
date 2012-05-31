07b173188f1539e58ac3dbb0d46081eb1d9ce7dd
gentinex <gentinex@yahoo.com>
2012-05-31 09:00:08 -0400

    overhauled backtest routine; added cross-validation

--- TRACEBondPrice/source/backtest.r
+++ TRACEBondPrice/source/backtest.r
@@ -2,48 +2,96 @@
 source('utils.r')
 source('constant.r')
 source('kalman_filter.r')
+source('simple_regression.r')
 
 BacktestUnsupervised <- function(){
   time <- proc.time()
   print('Reading the data...')
-  train <-read.csv( '../data/train.csv',  header = TRUE, na.strings = 'NA')[1:10,]
+  train <-read.csv( '../data/train.csv',  header = TRUE, na.strings = 'NA')
   time <- RecordTime(time)
 
   print('Making constant prediction...')
   predictions.constant = Constant(train)
-  mse.constant <- mean((predictions.constant$trade_price - train$trade_price) ** 2)
-  print(paste('MSE is', mse.constant))
-  write(mse.constant, '../backtest/mse.constant')
+  print(paste('Weighted MSE is', WeightedMSE(predictions.constant, train)))
   time <- RecordTime(time)
 
   print('Making basic Kalman Filter prediction...')
-  prediction.kalman.id = KalmanFilter(train, simple=TRUE)
-  print(prediction.kalman.id)
-  print(train$trade_price)
-  mse.kalman.id <- mean((prediction.kalman.id$trade_price - train$trade_price) ** 2)
-  print(paste('MSE is', mse.kalman.id))
-  write(mse.kalman.id, '../backtest/mse.kalman.id')
+  predictions.kalman.id = KalmanFilter(train, simple=TRUE)
+  print(paste('Weighted MSE is', WeightedMSE(predictions.kalman.id, train)))
   time <- RecordTime(time)
 
   print('Making sqrt Kalman Filter prediction...')
-  prediction.kalman.sqrt = KalmanFilter(train, timemap=sqrt, simple=TRUE)
-  mse.kalman.sqrt <- mean((prediction.kalman.sqrt$trade_price - train$trade_price) ** 2)
-  print(paste('MSE is', mse.kalman.sqrt))
-  write(mse.kalman.sqrt, '../backtest/mse.kalman.sqrt')
+  predictions.kalman.sqrt = KalmanFilter(train, timemap=sqrt, simple=TRUE)
+  print(paste('Weighted MSE is', WeightedMDS(predictions.kalman.sqrt, train)))
   time <- RecordTime(time)
 
-  print('Making square Kalman Filter prediction...')
-  prediction.kalman.square = KalmanFilter(train, timemap=function(x) return(x*x), simple=TRUE)
-  mse.kalman.square <- mean((prediction.kalman.square$trade_price - train$trade_price) ** 2)
-  print(paste('MSE is', mse.kalman.square))
-  write(mse.kalman.square, '../backtest/mse.kalman.square')
-  time <- RecordTime(time)
-
-  print('Making log Kalman Filter prediction...')
-  prediction.kalman.log = KalmanFilter(train, timemap=log, simple=TRUE)
-  mse.kalman.log <- mean((prediction.kalman.log$trade_price - train$trade_price) ** 2)
-  print(paste('MSE is', mse.kalman.log))
-  write(mse.kalman.log, '../backtest/mse.kalman.log')
+  print('Making square Kalman Filter predictions...')
+  predictions.kalman.square = KalmanFilter(train, timemap=function(x) return(x*x), simple=TRUE)
+  print(paste('Weighted MSE is', WeightedMDS(predictions.kalman.square, train)))
   time <- RecordTime(time)
 }
 
+# fold a data set into training and test sets
+# example: say we are foldting a set with 34 elements,
+# and we fold into 10 test sets
+# floor(34/10) = 3
+# 3x + 4y = 34 and x + y = 10 is uniquely
+# solved by x = 6, y = 4, so we fold the data by making
+# the first 6 test sets each of size 3, and the last 4
+# test sets each of size 4
+# once we've determined a test set, the remaining data forms
+# the training set
+FoldData <- function(data, num.folds, fold.idx){
+  num.entries <- nrow(data)
+  stopifnot(fold.idx <= num.folds && fold.idx >= 1 && num.entries >= num.folds)
+  num.entries.base <- floor(num.entries / num.folds)
+  size.cutoff <- (num.entries.base + 1) * num.folds - num.entries
+  if(fold.idx <= size.cutoff){
+    num.test.entries <- num.entries.base
+    start.test.index <- 1 + num.test.entries * (fold.idx - 1)
+    end.test.index <- num.test.entries * fold.idx
+  }
+  else{
+    num.test.entries <- num.entries.base + 1
+    start.test.index <- 1 + size.cutoff * num.entries.base + num.test.entries * (fold.idx - size.cutoff - 1)
+    end.test.index <- size.cutoff * num.entries.base + num.test.entries * (fold.idx - size.cutoff)
+  }
+
+  test <- data[start.test.index:end.test.index, ]
+  train <- data[-start.test.index:-end.test.index, ]
+
+  return(list(train=train, test=test))
+}
+
+CrossValidate <- function(num.folds){
+  time <- proc.time()
+  print('Reading the data...')
+  train <-read.csv( '../data/train.csv',  header = TRUE, na.strings = 'NA')
+  time <- RecordTime(time)
+
+  for(i in 1:num.folds){
+    print(paste("Building cross validation set", i))
+    fold.data <- FoldData(train, num.folds, i)
+    used.train <- fold.data$train
+    used.test <- fold.data$test
+    time <- RecordTime(time)
+
+    print('Making Kalman filter sqrt prediction', i)
+    predictions.kalman.sqrt <- KalmanFilter(used.test, timemap=sqrt, simple=TRUE)
+    time <- RecordTime(time)
+
+    print('Making Kalman filter identity prediction', i)
+    predictions.kalman.identity <- KalmanFilter(used.test, timemap=identity, simple=TRUE)
+    time <- RecordTime(time)
+
+    #print('Making simple linear regression prediction', i)
+    #predictions.regression <- SimpleRegression(used.train, used.test)
+    #time <- RecordTime(time)
+
+    print(paste('Kalman sqrt weighted MSE is', WeightedMSE(predictions.kalman.sqrt, used.test)))
+    print(paste('Kalman identity weighted MSE is', WeightedMSE(predictions.kalman.identity, used.test)))
+    #print(paste('Regression weighted MSE is', WeightedMSE(predictions.regression, used.test)))
+    time <- RecordTime(time)
+  }
+
+}
